689511faa069b963d44cab43cf0e8537accff580
hhimanshu <harit.himanshu@yahoo.in>
2021-09-15 07:07:48 -0700

    feat: add RDI JSON and pass it to component in context

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,20 +5,21 @@
  */
 
 // You can delete this file if you're not using it
-const path = require('path')
-const data = require('./src/data/foundation_food_nutrition_facts.json')
+const path = require("path")
+const ff_nutrition_facts = require("./src/data/foundation_food_nutrition_facts.json")
+const rdi = require("./src/data/rdi.json")
 
-exports.createPages = ({actions}) => {
+exports.createPages = ({ actions }) => {
   const { createPage } = actions
-  const template = path.resolve('./src/templates/FoundationFoodNutritionFacts.tsx')
-  data.forEach(food => {
-    console.log(food)
-    const pagePath = food["fdcId"].toString()
+  const template = path.resolve("./src/templates/FoundationFoodNutritionFacts.tsx")
+  ff_nutrition_facts.forEach(food => {
+    const pagePath = food["name"].toString().replace(/[^\w]+/g, "_")
     createPage({
       path: pagePath,
       component: template,
       context: {
-        food
+        food,
+        rdi
       }
     })
   })
--- src/templates/FoundationFoodNutritionFacts.tsx
+++ src/templates/FoundationFoodNutritionFacts.tsx
@@ -2,11 +2,21 @@ import * as React from "react"
 import { graphql } from "gatsby"
 
 export default ({ pageContext }) => {
-  const { food } = pageContext
-  return <>
-    <h1>{food.name}</h1>
-    <pre>{JSON.stringify(food, null, 2)}</pre>
-  </>
+  const { food, rdi } = pageContext
+  return <div style={{
+    display: "flex",
+    justifyContent: "space-around",
+    marginTop: "5%"
+  }}>
+    <div>
+      <h1>{food.name}</h1>
+      <pre style={{ height: 600, width: "auto" }}>{JSON.stringify(food, null, 2)}</pre>
+    </div>
+    <div>
+      <h1>RDI</h1>
+      <pre style={{ height: 600, width: "auto" }}>{JSON.stringify(rdi, null, 2)}</pre>
+    </div>
+  </div>
 }
 
 export const query = graphql`
