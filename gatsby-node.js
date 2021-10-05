be394f91e7b630b263765c7950dcad058c01ae23
hhimanshu <harit.himanshu@yahoo.in>
2021-10-05 10:57:17 -0700

    feat(DEV-54): migrate gatsby-node.js from JavaSscript to TypeScript

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,11 +4,25 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-const {
-  generateFoundationFoodNutritionFactTables,
-  generateFoundationFoodPage
-} = require("./src/generators/usda/foundation_food")
+// adapted using https://gist.github.com/clarkdave/53cc050fa58d9a70418f8a76982dd6c8#gistcomment-3064797
 
+'use strict'
+
+require('ts-node').register({
+  compilerOptions: {
+    module: 'commonjs',
+    target: 'es2017',
+  },
+})
+
+exports.createPages = require('./src/gatsby/createPages').createPages
+
+// const {
+//   generateFoundationFoodNutritionFactTables,
+//   generateFoundationFoodPage
+// } = require("./src/generators/usda/foundation_food")
+
+/*
 exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
   generateFoundationFoodNutritionFactTables(createPage)
@@ -24,4 +38,4 @@ exports.createPages = async ({ actions, graphql }) => {
     }
    }`)
   generateFoundationFoodPage(createPage, data)
-}
+}*/
---
+++ src/gatsby/createPages.ts
@@ -0,0 +1,23 @@
+import { GatsbyNode } from "gatsby"
+
+const {
+  generateFoundationFoodNutritionFactTables,
+  generateFoundationFoodPage
+} = require("../generators/usda/foundation_food")
+
+
+export const createPages: GatsbyNode["createPages"] = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  generateFoundationFoodNutritionFactTables(createPage)
+  const { data } = await graphql(`
+  query {
+    allFoundationFoodNutritionFactsJson {
+      nodes {
+        name
+        category
+      }
+    }
+   }`)
+  generateFoundationFoodPage(createPage, data)
+}
