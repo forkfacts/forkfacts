c76ae2ac6f949741f5ef7037200582dfef185690
hhimanshu <harit.himanshu@yahoo.in>
2021-10-20 10:00:56 -0700

    feat(#17): use Chakra in Storybook

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,6 @@ import {ReactComponent as Logo} from "../../images/svg/logo.svg"
 export const Header = () => {
     return <Flex py={2} pl={2} border={'1px solid black'} gridGap={2} alignItems={"center"}>
         <Logo height={32} width={32}/>
-        <Text fontSize={'xl'} fontWeight={"black"}>TheBigLogo</Text>
+        <Text fontSize={'xl'} fontWeight={'extrabold'}>forkfacts</Text>
     </Flex>
 }
