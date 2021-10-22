cef92986c4ae80f7fa994cdea753cb4dc939dbc7
hhimanshu <harit.himanshu@yahoo.in>
2021-10-22 12:54:27 -0700

    feat(#23): add basic FactTable without types resolves #23

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import { theme } from "@chakra-ui/react"
 import "@fontsource/inter/400.css"
+import "@fontsource/inter/500.css"
 import "@fontsource/inter/700.css"
 import "@fontsource/inter/900.css"
 
--- src/components/FactTable/index.tsx
+++ src/components/FactTable/index.tsx
@@ -1,5 +1,76 @@
 import React from "react"
+import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid"
+import { Box, Text } from "@chakra-ui/react"
+import { makeStyles } from "@material-ui/core"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& .MuiDataGrid-columnsContainer": {
+      backgroundColor: "black",
+      color: "white",
+    },
+    "& .MuiDataGrid-sortIcon": {
+      fill: "white",
+    },
+    "& .MuiDataGrid-columnSeparator": {
+      backgroundColor: "black",
+    },
+    "& .MuiDataGrid-iconSeparator": {
+      fill: "black",
+    },
+  },
+}))
+
+const rows: GridRowsProp = [
+  { id: 1, nutrient: "Hello", amount: "World", dailyValue: "2.68%" },
+  { id: 2, nutrient: "DataGridPro", amount: "is Awesome", dailyValue: "4.5 %" },
+  { id: 3, nutrient: "MUI", amount: "is Amazing", dailyValue: "9.1 %" },
+  /*
+      {id: 4, nutrient: "MUI", amount: "is Amazing", dailyValue: "9.1 %"},
+      {id: 5, nutrient: "MUI", amount: "is Amazing", dailyValue: "9.1 %"},
+      {id: 6, nutrient: "MUI", amount: "is Amazing", dailyValue: "9.1 %"},
+  */
+]
+
+const getHeader = (name: string) => <Text fontWeight={"bold"}>{name}</Text>
+const columns: GridColDef[] = [
+  {
+    field: "nutrient",
+    flex: 1,
+    renderHeader: params => getHeader("Nutrient"),
+  },
+  {
+    field: "amount",
+    width: 150,
+    renderHeader: params => getHeader("Amount"),
+  },
+  {
+    field: "dailyValue",
+    flex: 1,
+    renderHeader: params => getHeader("% Daily Value"),
+  },
+]
 
 export const FactTable = () => {
-  return <h1>Coming Soon!!</h1>
+  const classes = useStyles()
+  return (
+    <div style={{ height: 500, width: "100%" }}>
+      <DataGrid
+        autoHeight={true}
+        rows={rows}
+        columns={columns}
+        className={classes.root}
+        components={{
+          // https://mui.com/api/data-grid/data-grid/#slots-2
+          Footer: () => (
+            <Box py={4} pl={3}>
+              <Text fontSize={"xs"} color={"gray.500"}>
+                27 Nutrients
+              </Text>
+            </Box>
+          ),
+        }}
+      />
+    </div>
+  )
 }
