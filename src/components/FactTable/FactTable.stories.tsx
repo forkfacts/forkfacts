ff37d6c935108aaaaf61c05b12e01e8813ec27df
hhimanshu <harit.himanshu@yahoo.in>
2021-10-22 12:56:08 -0700

    feat(#23): added stories for desktop, mobile, and tablet resolves #23

--- src/components/FactTable/FactTable.stories.tsx
+++ src/components/FactTable/FactTable.stories.tsx
@@ -1,8 +1,29 @@
 import { FactTable } from "./index"
+import React from "react"
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport"
 
 export default {
   title: "Components/FactTable",
   component: FactTable,
+  parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+    },
+  },
 }
 
-export const Facts = () => <FactTable />
+export const Desktop = () => <FactTable />
+
+export const Mobile = () => <FactTable />
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: "pixel",
+  },
+}
+
+export const Tablet = () => <FactTable />
+Tablet.parameters = {
+  viewport: {
+    defaultViewport: "ipad",
+  },
+}
