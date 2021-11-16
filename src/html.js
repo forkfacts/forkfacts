2b79bdbc34eb4619fcdb5ea605b6a63824c793d9
Harit Himanshu <hhimanshu@users.noreply.github.com>
2021-11-15 17:04:39 -0800

    fix(#15): add meta tag for yandex verification (#56)
    resolves #15

--- src/html.js
+++ src/html.js
@@ -16,6 +16,7 @@ export default function HTML(props) {
           data-domain="forkfacts.app"
           src="https://plausible.io/js/plausible.js"
         ></script>
+        <meta name="yandex-verification" content="77e4a308c69c5510" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
