f06ccf75555ba463d302f8ee87b8d74bb0eebbc9
deekshasharma <deeksha.sharma25@gmail.com>
2021-12-21 18:11:39 -0800

    feat(#64): updates css for autocomplete

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import "./autocomplete.css"
 import { getAlgoliaResults } from "@algolia/autocomplete-js"
 import algoliasearch from "algoliasearch"
 import { Autocomplete } from "./Autocomplete"
@@ -10,30 +11,33 @@ const apiKey = process.env.GATSBY_SEARCH_API_KEY
 const searchClient = algoliasearch(appId, apiKey)
 function App() {
   return (
-    <Autocomplete
-      openOnFocus={true}
-      getSources={({ query }) => [
-        {
-          sourceId: "products",
-          getItems() {
-            return getAlgoliaResults({
-              searchClient,
-              queries: [
-                {
-                  indexName: "ff_index",
-                  query,
-                },
-              ],
-            })
-          },
-          templates: {
-            item({ item, components }) {
-              return <SearchResult hit={item} components={components} />
+    <div className="root">
+      <Autocomplete
+        placeholder="Search for any food"
+        openOnFocus={true}
+        getSources={({ query }) => [
+          {
+            sourceId: "products",
+            getItems() {
+              return getAlgoliaResults({
+                searchClient,
+                queries: [
+                  {
+                    indexName: "ff_index",
+                    query,
+                  },
+                ],
+              })
+            },
+            templates: {
+              item({ item, components }) {
+                return <SearchResult hit={item} components={components} />
+              },
             },
           },
-        },
-      ]}
-    />
+        ]}
+      />
+    </div>
   )
 }
 
