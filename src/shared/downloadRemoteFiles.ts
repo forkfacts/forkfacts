172134c2c85080e1128352e4336ce99b68701d57
hhimanshu <harit.himanshu@yahoo.in>
2021-12-22 10:29:25 -0800

    fix(#77): restore development data files after creating production build.

--- src/shared/downloadRemoteFiles.ts
+++ src/shared/downloadRemoteFiles.ts
@@ -1,11 +1,12 @@
 const { execSync } = require("child_process")
 
+const DOWNLOAD_DIRECTORY = "src/data"
 const downloadRemoteFiles = () => {
   const remoteURL = process.env.API_URL
   if (!remoteURL) throw "No API_URL configured to download project files"
   console.log(`downloading files from ${remoteURL}`)
   const command = `
-  cd src/data &&\
+  cd ${DOWNLOAD_DIRECTORY} &&\
   wget --no-check-certificate "${remoteURL}" -r -A 'uc*' -e robots=off -nd -O files.zip &&\
   unzip -o files.zip
   `
