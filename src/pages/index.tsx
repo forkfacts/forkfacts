c25552f1484f55a3569d16df47014a98f43b8d34
hhimanshu <harit.himanshu@yahoo.in>
2021-12-31 17:52:31 -0800

    feat(#68): align search box based on responsive design (wip)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,49 @@
 import * as React from "react";
 import Seo from "../components/seo";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, useBreakpoint } from "@chakra-ui/react";
 import { theme } from "../theme";
 import { Home } from "../components/Home";
+import { ResponsiveBreakpoint } from "../components/utils";
 
-const Index = () => (
-  <ChakraProvider theme={theme}>
-    <Seo title="ForkFacts" />
-    <div
-      className="container"
-      style={{
-        display: "grid",
-        height: "95vh",
-        gridTemplateColumns: "repeat(4, 1fr)",
-        gap: "10px",
-        gridTemplateRows: "repeat(4, 1fr)",
-        gridTemplateAreas: `
+const Index = () => {
+  const breakpoint = useBreakpoint() as ResponsiveBreakpoint;
+  const sm = breakpoint === "sm";
+  const base = breakpoint === "base";
+  // todo: this does not seem to be working
+  const columnWidth = sm || base ? "0.2fr 2fr 2fr 0.2fr" : "repeat(4, 1fr)";
+  return (
+    <ChakraProvider theme={theme}>
+      <Seo title="ForkFacts" />
+      <div
+        className="container"
+        style={{
+          display: "grid",
+          height: "95vh",
+          gridTemplateColumns: columnWidth,
+          gap: "10px",
+          gridTemplateRows: "repeat(4, 1fr)",
+          gridTemplateAreas: `
         ". Content Content ."
         ". Content Content ."
         ". Content Content ."
         ". Content Content ."
         `,
-      }}
-    >
-      <div
-        className="Content"
-        style={{
-          gridArea: "Content",
-          alignSelf: "center",
-          justifySelf: "center",
-          width: "100%",
         }}
       >
-        <Home />
+        <div
+          className="Content"
+          style={{
+            gridArea: "Content",
+            alignSelf: "center",
+            justifySelf: "center",
+            border: "0px solid blue",
+          }}
+        >
+          <Home />
+        </div>
       </div>
-    </div>
-  </ChakraProvider>
-);
+    </ChakraProvider>
+  );
+};
 
 export default Index;
