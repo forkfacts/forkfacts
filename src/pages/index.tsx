25f648f3db59a496fb0b9f3128c4516c6c0e01f3
deekshasharma <deeksha.sharma25@gmail.com>
2021-12-31 16:44:42 -0800

    feat(#68): adds css grid to center the content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,37 @@
 import * as React from "react";
-import { graphql, PageProps } from "gatsby";
-
-import Layout from "../components/Layout";
 import Seo from "../components/seo";
-import { Box, Center, Text } from "@chakra-ui/react";
 
-type DataProps = {
-  site: {
-    buildTime: string;
-  };
-};
-
-const Index: React.FC<PageProps<DataProps>> = ({ data, path }) => (
-  <Layout>
+const Index = () => (
+  <>
     <Seo title="ForkFacts" />
-    <Box
+    <div
+      className="container"
       style={{
         display: "grid",
-        gridTemplateRows: "repeat(3, 1fr)",
-        gridTemplateColumns: "repeat(1, 1fr)",
+        height: "95vh",
+        gridTemplateColumns: "repeat(4, 1fr)",
+        gap: "10px",
+        gridTemplateRows: "repeat(4, 1fr)",
+        gridTemplateAreas: `
+        ". Content Content ."
+        ". Content Content ."
+        ". Content Content ."
+        ". Content Content ."
+        `,
       }}
     >
-      <Text
-        fontSize={"4xl"}
-        fontWeight={"extrabold"}
-        style={{ gridRowStart: 1 }}
+      <div
+        className="Content"
+        style={{
+          gridArea: "Content",
+          alignSelf: "center",
+          justifySelf: "center",
+        }}
       >
-        Democratizing Nutrition information
-      </Text>
-      <Text fontSize={"2xl"} style={{ gridRowStart: 2 }}>
-        <i>... one food at a time</i>
-      </Text>
-      <Center bg="gray.100" color="white" style={{ gridRowStart: 3 }} p={4}>
-        <Text color={"black"}>
-          We are a small team working hard to provide <b>fact-based</b>{" "}
-          nutrition information for every possible food out there. Check out
-          nutrition information for{" "}
-          <a href="/foods" style={{ color: "blue" }}>
-            some foods
-          </a>{" "}
-          here. Come visit us in some time for more information and news.
-        </Text>
-      </Center>
-    </Box>
-  </Layout>
+        Hello World
+      </div>
+    </div>
+  </>
 );
 
 export default Index;
-
-export const query = graphql`
-  {
-    site {
-      buildTime(formatString: "YYYY-MM-DD hh:mm a z")
-    }
-  }
-`;
